b00fb62c50f857d08ca4d50dd44cb57055cb2246
ronaldo0702 <55457687+ronaldo0702@users.noreply.github.com>
2019-11-18 19:12:23 -0600

    actualizado

--- integradora_pagina_web/php/consesion.php
+++ integradora_pagina_web/php/consesion.php
@@ -1,35 +1,36 @@
 <?php
 session_start();
 use clases_pdo\User;
-require_once('../clases/usuario.php');
+require_once('usuario.php');
 $users = new User();
 
-if (!isset($_POST['correo']) || !isset($_POST['contrasena'])){
+if (!isset($_POST['correo_electronico']) || !isset($_POST['contrasena'])){
     echo "contra o usuario no son valido";
     //header("Location:../view/index.php?c1=error");
 }
-else if (empty($_POST['correo']) || empty($_POST['contrasena'])){
+else if (empty($_POST['correo_electronico']) || empty($_POST['contrasena'])){
             echo "campos vacios ";
-            // header("Location:../view/index.php?c2=error");    
+            // header("Location:../view/index.php?c2=error"); 
+
+               header("Location:../index.html"); 
 }
 else{        
-            $correo = $_POST['correo'];
+            $correo = $_POST['correo_electronico'];
             $contrasena = $_POST['contrasena'];
             $result = $users->selectVali($correo);
-            if ($result['correo'] == $correo)
+            if ($result['correo_electronico'] == $correo)
             {
                 if ($result['contrasena'] == $contrasena)
                 {
-                    $_SESSION['idUsuario']=$result['id_Per'];
-                    $_SESSION['usuario'] = $result['nombres'];
-                    $_SESSION['rol']=$result['rol'];
-                    header("Location:../../index.php");
+                    $_SESSION['id_usuario']=$result['id_usuario'];
+                    $_SESSION['usuario'] = $result['nombre'];
+                    header("Location: ../index.html");
                 }else{
-                    header("Location:../../Iniciodesesion.php");
+                    header("Location: ../index.html");
                     //header("Location:../view/index.php?c3=error");
                 }
             }else{
                 echo "el usuario no es correcto";
-                header("Location: Refresh:3; ../../Iniciodesesion.php");
+                header("Location: Refresh:3; ../index.html");
             }            
         }
