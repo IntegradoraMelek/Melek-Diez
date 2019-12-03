44646fb4fe25e55c4d604c17c1ade293aa3a1249
ronaldo0702 <55457687+ronaldo0702@users.noreply.github.com>
2019-12-03 17:54:03 -0600

    Cambios

--- regusu.php
+++ regusu.php
@@ -1,6 +1,7 @@
 <?php   
 use clases_pdo\User;
 require_once('php/usuario.php');
+
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $correo_electronico = $_POST['correo_electronico'];
@@ -11,6 +12,11 @@ $contrasena = $_POST['contrasena'];
 $Telefono = is_valid_phone($phone);
 $Correo = is_confirm_pass($pass,$pass_b);
 */
+echo $nombre;
+echo $apellido;
+echo $correo_electronico;
+echo $telefono;
+echo $contrasena;
 $user = new User();
 $result = $user->agregarUsuario($nombre,$apellido,$correo_electronico,$telefono,$contrasena);
 if ($result=="1") {
