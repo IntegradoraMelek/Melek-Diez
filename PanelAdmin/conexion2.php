151fac4cfabaf53a86ab7ab3f62617d3ad3ad194
GerardoDM <geradelgado22@hotmail.com>
2019-12-08 10:57:42 -0600

    cambié conexion2, no se podia conectar a servidor

--- PanelAdmin/conexion2.php
+++ PanelAdmin/conexion2.php
@@ -1,13 +1,19 @@
 <?php
 
-$password = "user";
-$user = "melek_user";
-$dbname = 'melek_melek3';
+// $password = "user";
+// $user = "melek_user";
+// $dbname = 'melek_melek3';
+
+
+
+$password = "root";
+$user = "root";
+$dbname = 'melek3A';
 
 
 
 try{
-    $conn = new PDO('mysql:host=melek.uttics.com;dbname=' . $dbname,$user,$password);
+    $conn = new PDO('mysql:host=localhost;dbname=' . $dbname,$user,$password);
     echo "Conexion exitosa";
 }catch (Exception $e){
     echo "Conexión no exitosa: " . $e->getMessage();
