f3d60e993682627195e7491d45c65b3c93579817
GerardoDM <geradelgado22@hotmail.com>
2019-12-11 05:14:53 -0600

    cambie mi bd local para igualar parametros de clase

---
+++ php/conexion3.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace clases_pdo;
+
+class Conexion extends \PDO
+{
+    private $typeDB = 'mysql';
+    private $host = 'localhost';
+    private $dbname = 'melek3A';
+    private $root = 'root';
+    private $pass= 'root';   
+    
+    
+    public function __construct(){
+        try {
+            parent::__construct("$this->typeDB:host=$this->host;dbname=$this->dbname",$this->root,$this->pass);
+            $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);    
+        } catch (Exception $e) {
+            echo "DATA  BASE ERROR:".$e->getMessage();
+        }
+    }
+}
