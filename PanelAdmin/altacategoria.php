7cdf7cb708eec4b60757142a5a0b6a0c2ad5e2f5
ronaldo0702 <55457687+ronaldo0702@users.noreply.github.com>
2019-12-11 21:44:58 -0600

    solo checando

--- PanelAdmin/altacategoria.php
+++ PanelAdmin/altacategoria.php
@@ -1,18 +1,10 @@
 <?php
-
 require "conexion2.php";
-
 $nombre = $_POST["nombre_categoria"];
-
-
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-
 $query = 'INSERT INTO categoria (nombre_categoria) values ("'.$nombre.'")';
-
 // exit($query);
-
 $res = $conn->prepare($query);
-
 $res->execute();
-
 print_r($res->errorInfo());
+?>
