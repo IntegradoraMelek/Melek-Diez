42e1adc8014e2847e5df84c060f2191aac1ceab4
GerardoDM <geradelgado22@hotmail.com>
2019-12-12 13:25:20 -0600

    modifiqué alta producto

--- PanelAdmin/altaproducto.php
+++ PanelAdmin/altaproducto.php
@@ -6,13 +6,20 @@ $nombre = $_POST["nombre"];
 $precio = $_POST["precio"];
 $descripcion = $_POST["descripcion"];
 $categoria = $_POST["selectcategoria"];
+$imagen = $_FILES["imagen"]['name'];
 
-
+$Fecha= new DateTime();
+$conversion=($imagen!="")?$Fecha->getTimestamp()."_".$_FILES["imagen"]['name']:"default.jpg";
+    $temporalfoto= $_FILES["imagen"]["tmp_name"];
+    if($temporalfoto!=""){
+        move_uploaded_file($temporalfoto,"imagenes/".$conversion);
+    }
 
 
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
-$query = 'INSERT INTO producto (nombre_producto , precio_unitario , descripcion , id_categoria) values ("'.$nombre.'","'.$precio.'","'.$descripcion.'","'.$categoria.'")';
+
+$query = 'INSERT INTO producto (nombre_producto , precio_unitario , descripcion , id_categoria, imagen) values ("'.$nombre.'","'.$precio.'","'.$descripcion.'","'.$categoria.'","'.$conversion.'")';
 
 // exit($query);
 
