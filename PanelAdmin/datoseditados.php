5f093fe3b201896a03e58556dccb2935dcd2ddb9
GerardoDM <geradelgado22@hotmail.com>
2019-12-11 20:49:39 -0600

    corregí datos editados

--- PanelAdmin/datoseditados.php
+++ PanelAdmin/datoseditados.php
@@ -1,23 +1,24 @@
 <?php
 
-require 'conexion.php';
+require 'conexion2.php';
 
 $id = $_POST['id_producto'];
-$nombre = $_POST['nombre'];
-$color = $_POST['color'];
-$descripcion = $_POST['descripcion'];
+$nombre = $_POST["nombre"];
+$precio = $_POST["precio"];
+$descripcion = $_POST["descripcion"];
+$categoria = $_POST["selectcategoria"];
 
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
 
 
-$query = 'UPDATE producto set nombre = ?,
-color = ?, descripcion = ? where id_producto = ?;';
+$query = 'UPDATE producto set nombre_producto = ?,
+precio_unitario = ?, descripcion = ?, id_categoria = ? where id_producto = ?;';
 
 // exit($query);
 
 $res = $conn->prepare($query);
 
-$res->execute([$nombre, $color, $descripcion, $id]);
+$res->execute([$nombre, $precio, $descripcion, $categoria, $id]);
 
 print_r($res->errorInfo());
 
