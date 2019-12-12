555f31fbcf503602c05715eb5f1f8697875f19f2
GerardoDM <geradelgado22@hotmail.com>
2019-12-12 04:28:35 -0600

    cambio js

--- datospedido.js
+++ datospedido.js
@@ -1,28 +1,31 @@
 $( document ).ready(function() {
 
-$("#selectProducto").change(function(){
+   
+
+$("#selectproducto").change(function(){
 
     $("#preciopro").html($("option:selected",this).attr("data-precio"))
+    
 });
 
-function getPrecio() {
-        var str='';
-        var val=document.getElementById('selectProducto');
-        for (i=0;i< val.length;i++) { 
-            if(val[i].selected){
-                str += val[i].value + ','; 
-            }
-        }         
-        var str=str.slice(0,str.length -1);
+// function getPrecio() {
+//         var str='';
+//         var val=document.getElementById('selectProducto');
+//         for (i=0;i< val.length;i++) { 
+//             if(val[i].selected){
+//                 str += val[i].value + ','; 
+//             }
+//         }         
+//         var str=str.slice(0,str.length -1);
         
-	$.ajax({          
-        	type: "GET",
-        	url: "getprecio.php",
-        	data:'id_producto='+str,
-        	success: function(data){
-        		$("#precioUnitario").html(data);
-        	}
-	});
-}
+// 	$.ajax({          
+//         	type: "GET",
+//         	url: "getprecio.php",
+//         	data:'id_producto='+str,
+//         	success: function(data){
+//         		$("#precioUnitario").html(data);
+//         	}
+// 	});
+// }
 });
 
